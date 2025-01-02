ef512e0a75965ea9b632edfc6e4ba39928b43d7d
harikrishnaa5 <harikrishnaswathi5@gmail.com>
2025-01-02 18:37:02 +0530

    query strings in route

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,13 +1,34 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Course } from '../Models/course';
-import { CourseService } from '../Services/course.service'
+import { CourseService } from '../Services/course.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css'],
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
   coursesService = inject(CourseService);
-  AllCourses: Course[] = this.coursesService.courses;
+  AllCourses: Course[];
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+  searchString: string;
+
+  ngOnInit() {
+    this.activeRoute.queryParamMap.subscribe((data) => {
+      console.log('Query param updated:', data.get('search'));
+      this.searchString = data.get('search');
+      this.updateCourseList();
+    });
+  }
+
+  updateCourseList() {
+    if (!this.searchString) {
+      this.AllCourses = this.coursesService.courses;
+    } else {
+      this.AllCourses = this.coursesService.courses.filter((course) =>
+        course.title.toLowerCase().includes(this.searchString.toLowerCase())
+      );
+    }
+  }
 }
--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-banner',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class BannerComponent {
 
+  router: Router = inject(Router)
+ onButtonClick(value: string) {
+
+  //  [queryParams]="{search: searchVar.value}"
+  this.router.navigate(['/Courses'], {  queryParams :{search: value}})
+ }
 }
