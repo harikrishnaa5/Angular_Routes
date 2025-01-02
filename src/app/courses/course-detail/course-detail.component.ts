03f38dcdc750e8cec7c0789937fbc59e4f805694
harikrishnaa5 <harikrishnaswathi5@gmail.com>
2025-01-02 14:14:35 +0530

    route parameter

--- src/app/courses/course-detail/course-detail.component.ts
+++ src/app/courses/course-detail/course-detail.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from 'src/app/Models/course';
+import { CourseService } from 'src/app/Services/course.service';
 
 @Component({
   selector: 'app-course-detail',
   templateUrl: './course-detail.component.html',
   styleUrls: ['./course-detail.component.css']
 })
-export class CourseDetailComponent {
+export class CourseDetailComponent implements OnInit {
+  selectedCourse: Course;
+  courseId: number;
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+  courseService: CourseService = inject(CourseService)
+
+  ngOnInit() {
+    // this.courseId = this.activeRoute.snapshot.params['id'] // returns the value of current id route parameter
+    this.courseId = +this.activeRoute.snapshot.paramMap.get('id')
+    this.selectedCourse = this.courseService.courses.find(course => course.id === this.courseId)
+  }
+
 
 }
